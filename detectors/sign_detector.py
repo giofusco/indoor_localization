added noise, fixed double call to initialization function. TODO: finish integration of exit sign detector

detectors/sign_detector.py
@@ -1,16 +1,17 @@
-from observer import IObserver
-import data_constants as dc
+# from observer import IObserver
+from input import data_constants as dc
 import numpy as np
 import cv2
 import camera_info
-import svmutil
+# import svmutil
 import math
 import camera_info
+from classifiers import svmutil
 
 DETECTION_WINDOW_NAME = "SignDetector"
 
 
-class SignDetector(IObserver):
+class SignDetector:
 
     def __init__(self, name, camera_id = camera_info.IPHONE8_640x360):
         # init descriptors
@@ -34,7 +35,13 @@ class SignDetector(IObserver):
 
         self.fx = self.camera_matrix[0][0,0]
         self.fy = self.camera_matrix[1][0,1]
+        self.observed_distance_to_sign = -1.
 
+
+    def get_observation(self, data):
+        self.observed_distance_to_sign = -1.
+        self.update(data)
+        return self.observed_distance_to_sign
 
     def extract_features_halves(self, img, feature_extractor):
         h, w, _ = img.shape
@@ -47,7 +54,7 @@ class SignDetector(IObserver):
         des.append(d)
 
         k, d = feature_extractor.detectAndCompute(img[0:h, int(w / 2) + 1:w], None)
-        for i in xrange(0, len(k)):
+        for i in range(0, len(k)):
             k[i].pt = (k[i].pt[0] + int(w / 2) + 1, k[i].pt[1])
 
         # cv2.drawKeypoints(sift_img, k, sift_img)
@@ -111,7 +118,7 @@ class SignDetector(IObserver):
 
                     r_hog = self.compute_hog(gray, r, self.hog)
                     p_labels, p_acc, p_vals = svmutil.svm_predict([1], r_hog.transpose().tolist(), self.svm, '-b 1')
-                    print p_vals
+                    print (p_vals)
                     if p_vals[0][0] > 0.5:
                         # cv2.rectangle(output_img, (r[0], r[1]), (r[0] + r[2], r[1] + r[3]), (255, 0, 0), 2)
                         # cv2.imshow("Stage2", output_img)
@@ -159,7 +166,7 @@ class SignDetector(IObserver):
         hh = Ihsv[:,:,0] >= ht
         hs = Ihsv[:, :, 1] >= st
         M = hh & hs
-        print st, ht
+        # print st, ht
         rect_element = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
         closing = cv2.morphologyEx(M.astype(np.uint8)*255 , cv2.MORPH_CLOSE, rect_element, iterations=2)
         horizontal_sum = np.sum(closing/255, 1)
@@ -181,10 +188,10 @@ class SignDetector(IObserver):
             else:
                 break
         Z = self.fy * 0.2032 / np.sum(horizontal_sum>0)
-        print Z
-        cv2.imshow("ROI_filt", closing)
-        cv2.imshow("ROI", M.astype(np.uint8)*255)
+        self.observed_distance_to_sign = Z
+        # cv2.imshow("ROI_filt", closing)
+        # cv2.imshow("ROI", M.astype(np.uint8)*255)
 
-        cv2.waitKey(-1)
+        # cv2.waitKey(-1)
 
 

particlefilter/odometry.py
@@ -60,18 +60,18 @@ class Odometry:
                                 marker_position_XY, yaw_marker = marker_detector.get_observations(self.annotated_map)
                                 if marker_position_XY is not None:
                                     # find out what is the VIO theta_0
-                                    yaw = yaw_marker - current_data[dconst.CAMERA_ROTATION][1]
+                                    # yaw = yaw_marker - current_data[dconst.CAMERA_ROTATION][1]
                                     found = True
                                     self.last_processed_timestamp = current_data[dconst.TIMESTAMP]
                                     self.starting_position = marker_position_XY
-                                    self.starting_yaw = yaw
+                                    self.starting_yaw = yaw_marker
                                     self.current_position = marker_position_XY
                                     self.current_VIO_position = np.array([current_data[dconst.CAMERA_POSITION][0],
                                                                  current_data[dconst.CAMERA_POSITION][2]])
                                     self.current_VIO_yaw = current_data[dconst.CAMERA_ROTATION][1]
-                                    self.VIO_yaw_offset = yaw - self.current_VIO_yaw
-                                    self.current_abs_yaw = yaw
-                                    print("Initial YAW: ", yaw)
+                                    self.VIO_yaw_offset =  current_data[dconst.CAMERA_ROTATION][1] - self.starting_yaw
+                                    self.current_abs_yaw = yaw_marker
+                                    print("Initial YAW: ",  yaw_marker)
                     else:
                         if current_data[dconst.IMAGE] is not None:
                             marker_detector.update(current_data)

particlefilter/particle_filter.py
@@ -12,8 +12,12 @@ PF_NOT_VALID = -2
 
 PF_X = 0
 PF_Z = 1
+PF_U = 0
+PF_V = 1
 PF_YAW = 2
 PF_SCORE = 3
+PF_DELTA_POS = 0
+PF_DELTA_YAW = 1
 
 class ParticleFilter:
 
@@ -30,6 +34,7 @@ class ParticleFilter:
         self.walkable_mask = self.annotated_map.get_walkable_mask()
         self.vis = visualizer
         self.yaw_offset = 0
+        self.tot_motion = 0.
 
 #todo: check if particle is outside of walkable area
     def initialize_particles_at(self, pos, yaw, position_noise_sigma=0.5, yaw_noise_sigma=0.1):
@@ -62,6 +67,7 @@ class ParticleFilter:
             sample_x, sample_z, num_valid = self.remove_non_walkable_locations(sample_x, sample_z)
             n_valid_particles += num_valid
             yaws = yaw_offset + np.random.normal(0, yaw_noise_sigma, num_valid)
+            # yaws = yaw_offset + np.random.normal(0, 0, num_valid)
             weights = np.ones(num_valid)
             num_samples = self.num_particles - n_valid_particles
             if len(self.particles) == 0:
@@ -74,12 +80,15 @@ class ParticleFilter:
 
     def step(self, measurements_deltas, observations):
         # print ("Particle Filter step")
-        self.move_particles_by(measurements_deltas[0], measurements_deltas[1],
-                               position_noise_sigma=0.01, yaw_noise_sigma=0.01)
+        self.move_particles_by(measurements_deltas[PF_DELTA_POS], measurements_deltas[PF_DELTA_YAW],
+                               position_noise_sigma=0.2, yaw_noise_sigma=0.01)
         # self.score_particles(observations)
         self.vis.plot_particles(annotated_map=self.annotated_map, particles=self.particles)
         # t0 = time.time()
-        self.resample_particles()
+        self.tot_motion += np.linalg.norm(measurements_deltas[PF_DELTA_POS], ord=2)
+        if  self.tot_motion >= 0.1:
+            self.resample_particles()
+            self.tot_motion = 0.
         # t1 = time.time()
         # print(t1-t0)
 
@@ -87,8 +96,9 @@ class ParticleFilter:
         tmp_pos = np.column_stack((x_vector, z_vector))
         tmp_pt = self.annotated_map.xy2uv_vectorized(tmp_pos)
 
-        sample_x = x_vector[self.walkable_mask[np.array(tmp_pt[:, 1]), np.array(tmp_pt[:, 0])] > 0]
-        sample_z = z_vector[self.walkable_mask[np.array(tmp_pt[:, 1]), np.array(tmp_pt[:, 0])] > 0]
+        idx = [self.walkable_mask[np.array(tmp_pt[:, PF_V]), np.array(tmp_pt[:, PF_U])] > 0]
+        sample_x = x_vector[idx]
+        sample_z = z_vector[idx]
 
         num_valid = len(sample_x)
 
@@ -98,23 +108,27 @@ class ParticleFilter:
                           check_wall_crossing=True):
 
         start_pt = (self.annotated_map.xy2uv_vectorized(self.particles[:, PF_X:PF_YAW]))
+        rotated_delta_pos = [ delta_pos[PF_X] * np.cos(self.particles[:,PF_YAW]) + -delta_pos[PF_Z] * np.sin(self.particles[:,PF_YAW]),
+                              -delta_pos[PF_X] * np.sin(self.particles[:, PF_YAW]) + -delta_pos[PF_Z] * np.cos(
+                                  self.particles[:, PF_YAW])]
 
-        pos_noise = self.__get_direction_noise(delta_pos, len(self.particles), sigma_pos=position_noise_sigma)
-        x = self.particles[:, PF_X] + delta_pos[0] * np.cos(self.particles[:,PF_YAW])  - delta_pos[1] * np.sin(self.particles[:,PF_YAW]) #+ pos_noise[0]
-        z = self.particles[:, PF_Z] - (delta_pos[0] * np.sin(self.particles[:, PF_YAW])  - delta_pos[1] * np.cos(self.particles[:, PF_YAW])) #- pos_noise[1]
+        n = rotated_delta_pos / np.linalg.norm(rotated_delta_pos, ord=2)
+        n_hat = [n[1], -n[0]]
 
-        # delta = np.linalg.norm(delta_pos, ord=2) + np.abs(np.random.normal(0, position_noise_sigma, len(self.particles)))
-        # x = self.particles[:, PF_X] - np.sin(self.particles[:, PF_YAW]) * delta
-        # z = self.particles[:, PF_Z] + np.cos(self.particles[:, PF_YAW]) * delta
+        # eps1 = np.random.normal(np.linalg.norm(rotated_delta_pos, ord=2) * .5, position_noise_sigma, len(self.particles))
+        # eps2 = np.random.normal(np.linalg.norm(rotated_delta_pos, ord=2) * .02, position_noise_sigma, len(self.particles))
+
+        eps1 = np.random.uniform(-np.linalg.norm(rotated_delta_pos, ord=2) * .95, np.linalg.norm(rotated_delta_pos, ord=2) * .95, len(self.particles))
+        eps2 = np.random.uniform(-np.linalg.norm(rotated_delta_pos, ord=2) * .02, np.linalg.norm(rotated_delta_pos, ord=2) * .02, len(self.particles))
+        noise = [eps1 * n[0], eps1 * n[1]] + [eps2 * n_hat[0], eps2 * n_hat[1]] #+ eps2 * n_hat
+
+
+
+        x = self.particles[:, PF_X] + rotated_delta_pos[PF_X] + noise[PF_X]
+        z = self.particles[:, PF_Z] + rotated_delta_pos[PF_Z] + noise[PF_Z]
+
         dest_pt = self.annotated_map.xy2uv_vectorized(np.column_stack((x, z)))
-
-        # self.vis.draw_points(self.annotated_map, start_pt, size=3, color=(0, 0, 255), title="Points - start")
-        # self.vis.draw_points(self.annotated_map, dest_pt, size = 3, color = (0, 0, 255), title = "Points")
-
         new_yaws = self.particles[:, 2] + np.random.normal(0, yaw_noise_sigma, len(self.particles))
-
-        # self.vis.plot_particle_displacement(annotated_map=self.annotated_map, particles=self.particles,
-        #                                        destinations_uv=dest_pt)
 
         if check_wall_crossing:
             self.particles = set_wall_hit_score(self.particles, start_pt, dest_pt, self.walls_image)
@@ -133,7 +147,6 @@ class ParticleFilter:
             self.particles[self.particles[:,PF_SCORE]>=0., PF_SCORE] = (1/(1+d[self.particles[:, PF_SCORE] >= 0.])).transpose()
 
     def resample_particles(self):
-        # if len(self.particles) > 0:
         tot_score = np.sum(self.particles[:, PF_SCORE])
         w = self.particles[:, PF_SCORE]
 
@@ -141,20 +154,15 @@ class ParticleFilter:
             w /= tot_score
         idx = np.random.choice(len(w), self.num_particles, [w])
         new_particles = self.particles[idx]
+
         self.particles = new_particles
         self.particles[:, PF_SCORE] = 1.
-        # self.particles[:, PF_YAW] += np.random.normal(0, yaw_noise_sigma, self.num_particles)
-        self.particles[:, PF_X] += np.random.normal(0, 0.1, self.num_particles)
-        self.particles[:, PF_Z] += np.random.normal(0, 0.1, self.num_particles)
-        print(len(self.particles))
-        # else:
-        #     raise RuntimeError("No particles left")
 
     @staticmethod
     def __get_direction_noise(delta_pos, num_samples, sigma_pos=0.1):
         tot_delta = np.sqrt(np.sum(delta_pos*delta_pos))
-        x_ratio = 2*(abs(delta_pos[0]) / tot_delta)
-        y_ratio = 2*(abs(delta_pos[1]) / tot_delta)
+        x_ratio = ((delta_pos[0]) / tot_delta)
+        y_ratio = ((delta_pos[1]) / tot_delta)
 
         # if abs(delta_pos[0]) < 0.025:
         #     x_ratio = 0
@@ -162,7 +170,7 @@ class ParticleFilter:
         #     y_ratio = 0
 
         noise = np.random.normal(0, sigma_pos, num_samples)
-        return abs(noise * x_ratio), abs(noise * y_ratio)
+        return (noise ), (noise )
         #return np.sign(delta_pos[0])*abs(noise*x_ratio), np.sign(delta_pos[1])*abs(noise*y_ratio)
 
 @jit(nopython=True)