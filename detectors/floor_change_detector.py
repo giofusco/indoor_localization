53fa6c6a4cc9f2a50ae955e950f1a52febd2ee21
Giovanni <giofusco@gmail.com>
2018-10-16 10:22:46 -0700

    Finished floor change detector

--- detectors/floor_change_detector.py
+++ detectors/floor_change_detector.py
@@ -1,5 +1,6 @@
 import numpy as np
 from input import data_constants as dc
+from math import floor
 
 class FloorChangeDetector:
 
@@ -9,12 +10,47 @@ class FloorChangeDetector:
     GONE_DOWN = 3
     STATIONARY = 5
 
-    def __init__(self, starting_floor_number, hPa_threshold = 0.35):
-        self.starting_floor_number = starting_floor_number
+    def __init__(self, starting_floor_number, hPa_threshold = 0.3):
+        self.current_floor_number = starting_floor_number
         self.hPa_threshold = hPa_threshold
         self.status = None
         self.previous_hpa = None
+        self.zero_count_threshold = 25
+        self.min_delta_val = 0.005
+        self.partial_delta_threshold = 0.1
+        self.p_Ref = None
+        self.prev_value = None
+        self.zero_count = 0
+        self.curr_direction = 0
+        self.prev_direction = 0
+        self.reset = False
 
     def update(self, data):
 
-        hPa = data[dc.BAROMETER] * 10
+        if data[dc.BAROMETER] is not None:
+            hPa = data[dc.BAROMETER] * 10
+
+            num_stories = 0.0
+
+            if self.reset:
+                self.p_Ref = hPa
+                self.reset = False
+
+            elif self.p_Ref is None:
+                self.p_Ref = hPa
+            else:
+                deltaP = (hPa - self.p_Ref)
+
+            local_delta = abs(hPa - self.prev_value)
+
+            if abs(deltaP) > self.hPa_threshold:
+                if local_delta < self.min_delta_val:
+                    self.reset = True
+                    num_stories = -(deltaP / abs(deltaP)) * floor(abs(deltaP) / self.hPa_threshold)
+                    self.current_floor_number += num_stories
+
+            self.prev_value = hPa
+
+            return int(self.current_floor_number, num_stories)
+        else:
+            raise RuntimeError("ERROR: No Barometer values available. Ending navigation system.")
